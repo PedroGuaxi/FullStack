17f9731cd692fa1fe94bc4146ea76ebdd7ffa1bf
Aquila <gabrielaquila15@gmail.com>
2023-09-29 16:52:53 -0300

    Teste 2 CRUD Lab

---
+++ SQLite/teste.py
@@ -0,0 +1,20 @@
+import database, sqlite3
+#create a table
+# 5 datatypes in SQLite : null(não existe), integer(number),real(decimal), text(texto),blob(mp3,image e etc.)
+# cursor.execute("""CREATE TABLE users (
+#                name DATATYPE text ,
+#                email DATATYPE text ,
+#                user_type_id DATATYPE integer,
+#                password DATATYPE text ,
+#                is_active DATATYPE integer ,
+#                cpf_cnpj DATATYPE text ,
+#                phone text DATATYPE integer
+# )
+# """)
+# cursor.execute("""CREATE TABLE lab (
+#                andar DATATYPE integer ,
+#                lab DATATYPE text ,
+#                description DATATYPE text,
+#                is_active DATATYPE integer 
+# )
+# """)
