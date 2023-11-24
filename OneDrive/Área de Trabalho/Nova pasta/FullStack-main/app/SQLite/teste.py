e596c5b4b65cf563fb30532840bfe32d87eb92bc
Mkrast <vieira.marcosrt@gmail.com>
2023-11-23 22:24:40 -0300

    Lista Labs

---
+++ OneDrive/Área de Trabalho/Nova pasta/FullStack-main/app/SQLite/teste.py
@@ -0,0 +1,27 @@
+# import database, sqlite3
+# #create a table
+# # 5 datatypes in SQLite : null(não existe), integer(number),real(decimal), text(texto),blob(mp3,image e etc.)
+# conection = sqlite3.connect('LabProject')
+# #create a cursor
+# cursor = conection.cursor()
+# cursor.execute("""CREATE TABLE users (
+#                name DATATYPE text ,
+#                email DATATYPE text ,
+#                user_type_id DATATYPE integer,
+#                password DATATYPE text ,
+#                is_active DATATYPE integer ,
+#                cpf_cnpj DATATYPE text ,
+#                phone text DATATYPE integer
+# )
+# """)
+# cursor.execute("""CREATE TABLE lab (
+#                andar DATATYPE integer ,
+#                lab DATATYPE text ,
+#                description DATATYPE text,
+#                is_active DATATYPE integer 
+# )
+# """)
+# # print(database.show_all_labs())
+# conection.commit()
+# #close our connection
+# conection.close()
